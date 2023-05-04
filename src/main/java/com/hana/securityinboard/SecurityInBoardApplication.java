805c353de795247d6f4398a480c0602cc815d20e
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-05-04 11:37:35 +0900

    Initial commit

---
+++ src/main/java/com/hana/securityinboard/SecurityInBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hana.securityinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityInBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityInBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hana/securityinboard/SecurityInBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.securityinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityInBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
