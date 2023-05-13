61430b4b6426a0bc8664fca74b915198c8ea5443
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-05-13 18:04:13 +0900

    #10 - 스프링 배치 구현을 위한 설정
    처음 구현해보는건데,
    잘 할 수 있을지 기대가 된다.

--- src/main/java/com/hana/securityinboard/SecurityInBoardApplication.java
+++ src/main/java/com/hana/securityinboard/SecurityInBoardApplication.java
@@ -1,8 +1,12 @@
 package com.hana.securityinboard;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableBatchProcessing
 @SpringBootApplication
 public class SecurityInBoardApplication {
 
